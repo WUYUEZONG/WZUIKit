e7456df566363386ca52b2a40611d94a153c0568
WUYUEZONG <w.yzong@outlook.com>
2021-10-13 14:58:54 +0800

    Initial commit

---
+++ Tests/WZUIKitTests/WZUIKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import WZUIKit
+
+final class WZUIKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(WZUIKit().text, "Hello, World!")
+    }
+}
