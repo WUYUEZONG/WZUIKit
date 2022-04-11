40615c2f26a70f7b1a96f45399569611640330e1
WUYUEZONG <w.yzong@outlook.com>
2022-04-11 10:56:34 +0800

    简化坐标

--- Sources/WZUIKit/WZUITools/Extensions/UIViewExtensions.swift
+++ Sources/WZUIKit/WZUITools/Extensions/UIViewExtensions.swift
@@ -15,4 +15,42 @@ public extension UIView {
         return Bundle.module.loadNibNamed(nibName, owner: owner, options: nil)?.first as? Self
     }
     
+    
+}
+
+/// 简化坐标
+public extension UIView {
+
+    var wzHeight: CGFloat {
+        self.frame.height
+    }
+    
+    var wzWidth: CGFloat {
+        self.frame.width
+    }
+    
+    var wzMinX: CGFloat {
+        self.frame.minX
+    }
+    
+    var wzMinY: CGFloat {
+        self.frame.minY
+    }
+    
+    var wzMaxX: CGFloat {
+        self.frame.maxX
+    }
+    
+    var wzMaxY: CGFloat {
+        self.frame.maxY
+    }
+    
+    var wzCenterX: CGFloat {
+        self.center.x
+    }
+    
+    var wzCenterY: CGFloat {
+        self.center.y
+    }
+    
 }
