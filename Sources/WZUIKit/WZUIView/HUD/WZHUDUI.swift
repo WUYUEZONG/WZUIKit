b70d23f9ba7e07b4515bab906e5e10cc407eddc3
WUYUEZONG <w.yzong@outlook.com>
2022-04-29 17:32:26 +0800

    1

--- Sources/WZUIKit/WZUIView/HUD/WZHUDUI.swift
+++ Sources/WZUIKit/WZUIView/HUD/WZHUDUI.swift
@@ -6,6 +6,28 @@
 //
 
 import SwiftUI
+import UIKit
+
+class UIKitViewController: UIViewController {
+    
+}
+
+struct UIKitViewControllerPresenter: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        return UIKitViewController()
+    }
+    
+}
+
+class UIKitHostingController: UIHostingController {
+    
+    override init(rootView: Content) {
+        super.init(rootView: rootView)
+    }
+    
+    
+}
 
 @available(iOS 13.0.0, *)
 public struct WZHUDUI : View {
@@ -16,8 +38,8 @@ public struct WZHUDUI : View {
     
     var image: UIImage?
     
-    var view: UIView {
-        UIHostingController(rootView: self).view
+    var viewController: UIHostingController {
+        UIHostingController(rootView: self)
     }
     
     public mutating func show(message: String?, image: UIImage?) {
@@ -25,17 +47,24 @@ public struct WZHUDUI : View {
         self.image = image
         if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
            let keyWindow = scene.windows.first {
-            if !keyWindow.subviews.contains(view) {
-                view.frame = keyWindow.bounds
-                keyWindow.addSubview(view)
-            } else {
-                keyWindow.bringSubviewToFront(view)
-            }
+            keyWindow.rootViewController?.show(viewController, sender: nil)
+//            if !keyWindow.subviews.contains(view) {
+//                view.frame = keyWindow.bounds
+//                keyWindow.addSubview(view)
+                
+//                view.translatesAutoresizingMaskIntoConstraints = false;
+//                view.centerXAnchor.constraint(equalTo: keyWindow.centerXAnchor).isActive = true
+//                view.centerYAnchor.constraint(equalTo: keyWindow.centerYAnchor).isActive = true
+                
+//            } else {
+//                keyWindow.bringSubviewToFront(view)
+//            }
         }
     }
     
     public func dismiss() {
-        view.removeFromSuperview()
+        viewController.dismiss(animated: true)
+        viewController.viewWillAppear(true)
     }
     
     
